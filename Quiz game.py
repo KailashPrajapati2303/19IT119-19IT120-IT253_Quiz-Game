d1ebd2df9d026e52ed8f8c4960f29dc3ad160b17
19IT120 <69593978+19IT120@users.noreply.github.com>
2020-09-14 14:56:42 +0530

    Add files via upload

--- Quiz game.py
+++ Quiz game.py
@@ -1,12 +1,177 @@
+def con():
+    print("\n\t\t\t\t\t\t\t\t\t>>>>>>>>>>>>>>><<<<<<<<<<<<<")
+    print('\n\t\t\t\t\t\t\t\t\t\t PRESS 1 Main Menu ')
+    print('\t\t\t\t\t\t\t\t\t\t PRESS 2 Exit\n')
+    f=int(input("Enter your choice : "))
+    if f==1:
+        switch()
+    else:
+        exit(1)
+def java():
+    score=0
+    print('\nQ.1  which of the following is the valid statement to create object? Consider Myclass.class is available')
+    print("1. Myclass m = new Myclass;\n2. Myclass m = Myclass();\n3. Myclass() m = new Myclass();\n4.  Myclass() m = new Myclass;\n")
+    w = int(input("Answer : "))
+    if w == 3:
+        print("correct answer")
+        score=score+1
+    else:
+        print("wrong answer")
+        print('Right answer : Myclass() m = new Myclass();')
+        score=score-0.25
+    #print('score=',score)
+
+    print('\n.......................\n')
+    print("\nQ.2 If no constructor is declared in the class then default constructor will be created by  Java Runtime Environment:\n")
+    print("1. True\n2. False\n")
+    w = int(input("Answer : "))
+    if w == 1:
+        print("correct answer")
+        score = score + 1
+    else:
+        print("wrong answer")
+        print('Right answer : True')
+        score = score - 0.25
+    #print('score=', score)
+
+    print('\n.......................\n')
+
+    print('\nQ.3 To declare class method, which of the following keyword is used?\n }')
+    print("1. static\n2. new\n3. public\n4. class\n")
+    w = int(input("Answer : "))
+    if w == 1:
+        print("correct answer")
+        score=score+1
+
+    else:
+        print("wrong answer")
+        print('Right answer : static')
+        score = score - 0.25
+    #print('score=', score)
+
+    print('\n.......................\n')
+
+    print('\nQ.4 Which of the following(s) is(are) valid declaration of java class?\n')
+    print("1. public class Myclass{}\n2. public class Myclass{int i:}\n3. public class Myclass{void demo(){}}\n4. All of the above\n")
+    w = int(input("Answer : "))
+    if w == 4:
+        print("correct answer")
+        score=score+1
+
+    else:
+        print("wrong answer")
+        print('Right answer : All of the above')
+        score = score - 0.25
+    #print('score=', score)
+
+    print('\n.......................\n')
+
+    print('\nQ.5 Which of the following is not a Java features?\n')
+    print("1. c Dynamic\n2. Architecture Neutral\n3. Use of pointers\n4. Object-oriented\n")
+    w = int(input("Answer : "))
+    if w == 3:
+        print("correct answer")
+        score=score+1
+
+    else:
+        print("wrong answer")
+        print('Right answer : Use of pointers')
+        score = score - 0.25
+    #print('score=', score)
+
+    print('\nQ.6 What is the use of the intern() method?')
+    print("1. It returns the existing string from memory\n2. It creates a new string in the database\n3. It modifies the existing string in the database\n4. None of the above\n")
+    w = int(input("Answer : "))
+    if w == 1:
+        print("correct answer")
+        score = score + 1
+    else:
+        print("wrong answer")
+        print("Right answer : It returns the existing string from memory")
+        score = score - 0.25
+    # print('score=',score)
+
+    print('\n.......................\n')
+    print(
+        "\nQ.7 What is the return type of the hashCode() method in the Object class?\n")
+    print("1. Object\n2. int\n3. long\n4. void\n")
+    w = int(input("Answer : "))
+    if w == 2:
+        print("correct answer")
+        score = score + 1
+    else:
+        print("wrong answer")
+        print('Right answer : int')
+        score = score - 0.25
+    # print('score=', score)
+
+    print('\n.......................\n')
+
+    print('\nQ.8 Which method of the Class.class is used to determine the name of a class represented by the class object as a String?\n')
+    print("1. getClass()\n2. intern()\n3. getName()\n4. toString()\n")
+    w = int(input("Answer : "))
+    if w == 3:
+        print("correct answer")
+        score = score + 1
+
+    else:
+        print("wrong answer")
+        print('Right answer : getName()')
+        score = score - 0.25
+    # print('score=', score)
+
+    print('\n.......................\n')
+
+    print('\nQ.9 Which package contains the Random class?\n')
+    print("1. java.util package\n2. java.lang package\n3. java.awt package\n4. java.io package\n")
+    w = int(input("Answer : "))
+    if w == 1:
+        print("correct answer")
+        score = score + 1
+
+    else:
+        print("wrong answer")
+        print('Right answer : java.util package')
+        score = score - 0.25
+    # print('score=', score)
+
+    print('\n.......................\n')
+
+    print('\nQ.10 An interface with no fields or methods is known as a ______.\n')
+    print("1. Runnable Interface\n2. Marker Interface\n3. Abstract Interface\n4. CharSequence Interface\n")
+    w = int(input("Answer : "))
+    if w == 2:
+        print("correct answer")
+        score = score + 1
+
+    else:
+        print("wrong answer")
+        print('Right answer : Marker Interface')
+        score = score - 0.25
+
+    print("\n>>>>>>>>>>><<<<<<<<<<<\n")
+    print('Final score=', score)
+    if score <= 0:
+        print("Final percentage : 0%")
+    else:
+        z = (float(score)) * 100 / 10
+        print("Final percentage : " + str(z) + "%")
+
+
 def c():
+    score=0
     print('\nQ.1 What Will be output of the following c program? \n #include\n int main()\n {\n  int print=12;\n  print("%d",print);\n  return 0;\n }')
     print("1. 12\n2. 0\n3. 5\n4. compilation error\n")
     w=int(input("Answer : "))
     if w==4:
         print("correct answer")
+        score=score+1
+
     else :
         print("wrong answer")
         print('Right answer : compilation error')
+        score = score -0.25
+   # print('score=', score)
 
     print('\n.......................\n')
     print("\nQ.2 The keyword used to transfer control from a function back to the calling function is\n")
@@ -14,9 +179,13 @@ def c():
     w = int(input("Answer : "))
     if w == 4:
       print("correct answer")
+      score = score + 1
+
     else:
       print("wrong answer")
       print('Right answer : return')
+      score = score - 0.25
+    #print('score=', score)
 
     print('\n.......................\n')
 
@@ -25,9 +194,13 @@ def c():
     w = int(input("Answer : "))
     if w == 3:
       print("correct answer")
+      score = score + 1
+
     else:
       print("wrong answer")
       print('Right answer : abcde')
+      score = score - 0.25
+    #print('score=', score)
 
     print('\n.......................\n')
 
@@ -36,9 +209,13 @@ def c():
     w = int(input("Answer : "))
     if w == 4:
       print("correct answer")
+      score = score + 1
+
     else:
       print("wrong answer")
       print('Right answer : strnchar()')
+      score = score -0.25
+    #print('score=', score)
 
     print('\n.......................\n')
 
@@ -47,18 +224,36 @@ def c():
     w = int(input("Answer : "))
     if w == 4:
       print("correct answer")
+      score = score + 1
+
     else:
       print("wrong answer")
       print('Right answer : c')
+      score = score - 0.25
+
+    print("\n>>>>>>>>>>><<<<<<<<<<<\n")
+    print('score=', score)
+    if score<=0:
+        print("Final percentage : 0%")
+    else:
+        q = (float(score)) * 100 / 5
+        print("Final percentage : " + str(q) + "%")
+
+
 def cplus():
+    score=0
     print('\nQ.1 Each pass through a loop is called a/an')
     print("1. enumeration\n2. iteration\n3. culmination\n4. pass through\n")
     s = int(input("Answer : "))
     if s == 2:
         print("correct answer")
+        score=score+1
+
     else:
         print("wrong answer")
         print('Right answer : iteration')
+        score = score - 0.25
+    #print('score=', score)
 
     print('\n.......................\n')
     print("\nQ.2 Which of the following is/are advantage of cellular partitioned structure:\n")
@@ -66,9 +261,13 @@ def cplus():
     s = int(input("Answer : "))
     if s == 3:
         print("correct answer")
+        score=score+1
+
     else:
         print("wrong answer")
         print('Right answer : Both a & b')
+        score = score - 0.25
+   # print('score=', score)
 
     print('\n.......................\n')
 
@@ -77,9 +276,13 @@ def cplus():
     s = int(input("Answer : "))
     if s == 1:
         print("correct answer")
+        score=score+1
+
     else:
         print("wrong answer")
         print('Right answer : case-sensitive')
+        score = score - 0.25
+   # print('score=', score)
 
     print('\n.......................\n')
 
@@ -88,9 +291,13 @@ def cplus():
     s = int(input("Answer : "))
     if s == 3:
         print("correct answer")
+        score=score+1
+
     else:
         print("wrong answer")
         print('Right answer : &&')
+        score = score - 0.25
+    #print('score=', score)
 
     print('\n.......................\n')
 
@@ -99,55 +306,74 @@ def cplus():
     s = int(input("Answer : "))
     if s == 2:
         print("correct answer")
+        score=score+1
+
     else:
         print("wrong answer")
         print('Right answer : ifstream')
+        score = score - 0.25
+
+    print("\n>>>>>>>>>>><<<<<<<<<<<\n")
+    print('Final score=', score)
+    if score <= 0:
+        print("Final percentage : 0%")
+    else:
+        v = (float(score)) * 100 / 5
+        print("Final percentage : " + str(v) + "%")
+
 
 
 
 def start(a):
 
-    print("\t\t>>>>>>>>>>>Start Quiz<<<<<<<<<<\n")
+    print("                                   >>>>>>>>>>>Start Quiz<<<<<<<<<<\n")
     print("                                           press 1 C\n")
     print("                                           press 2 C++\n")
     print("                                           press 3 Java\n")
 
     a = int(input("Enter Your Subject : "))
-  #  switcher={
-   #   1:c(),
-    #  2:cplus(),
-    #  3:"JAVA",
-   # }
+
 
     #return switcher.get(a,"Enter right choice")
     if a==1:
         c()
+        con()
     elif a==2:
         cplus()
+        con()
     elif a==3:
-        print("java")
+        java()
+        con()
     else:
         print("Enter Right choice")
-def switch(x):
-    if x==1:
+def switch():
+    print("                                    >>>>>>>>>>>QUIZ GAME<<<<<<<<<<\n")
+    print("                                           press 1 Start Game\n")
+    print("                                           press 2 About Game\n")
+    print("                                           press 3 Rules\n")
+    print("                                           press 4 Exit\n")
+    n = int(input("Enter Your Choice : "))
+
+
+    if n==1:
         start(1)
-    elif x==2:
-        print("about")
-    elif x==3:
+    elif n==2:
+        print("This Project created by Kailash Prajapati & Savan Prajapati in 3rd semester.")
+        con()
+    elif n==3:
+        print("if user pick right answer so user get 1 point sand if user pick Wrong answer so user lose 0.25 point.")
+        con()
+    elif n==4:
         exit(1)
     else:
         print("Enter Right choice")
 
 
+if __name__ == "__main__":
+ switch()
 
 
 
 
-print("                                    >>>>>>>>>>>QUIZ GAME<<<<<<<<<<\n")
-print("                                           press 1 Start Game\n")
-print("                                           press 2 About Game\n")
-print("                                           press 3 Exit\n")
-n=int(input("Enter Your Choice : "))
-c=switch(n)
-print(c)
+
 
